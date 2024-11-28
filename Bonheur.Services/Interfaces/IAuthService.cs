64a4d40ab5b94bbaee7c841850171f32e59feb19
phatnttse <127718651+phatnttse@users.noreply.github.com>
2024-11-29 00:57:45 +0700

    [Phatnttse][Update OpenID Connect][Update Custom Claims]

--- Bonheur.Services/Interfaces/IAuthService.cs
+++ Bonheur.Services/Interfaces/IAuthService.cs
@@ -8,11 +8,10 @@ namespace Bonheur.Services.Interfaces
 {
     public interface IAuthService
     {
-        Task<Microsoft.AspNetCore.Identity.SignInResult> CheckPasswordSignInAsync(ApplicationUser user, string password, bool lockoutOnFailure);
+        Task<ApplicationUser> HandleLoginAsync(string username, string password);
         Task<ClaimsPrincipal> CreateClaimsPrincipalAsync(ApplicationUser user, IEnumerable<string> scopes);
         Task<bool> CanSignInAsync(ApplicationUser user);
         Task<ApplicationUser> GetUserByIdAsync(string id);
-        Task<ApplicationUser> GetUserByUsername(string username);
         Task<ApplicationResponse> SignUpUserAccount(CreateAccountDTO createAccountDTO);
         Task<ApplicationResponse> ConfirmEmail(string email, string token);
         Task<ApplicationUser> HandleGoogleLoginAsync(GoogleAccountDTO googleAccountDTO);
