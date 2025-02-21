a40bd234f5f976aa2a788f9f2641cbd34ec381a0
phatnttse <127718651+phatnttse@users.noreply.github.com>
2025-02-21 10:56:09 +0700

    [Phatnttse][Update Supplier Entity][Update Supplier Address API]

---
+++ Bonheur.API/Migrations/20250221024946_Add_Longitude_Latitue_Supplier.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Bonheur.API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Add_Longitude_Latitue_Supplier : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Latitude",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Longitude",
+                table: "Suppliers",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Suppliers");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Suppliers");
+        }
+    }
+}
